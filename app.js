b8a07c971286cb6d5720acbda0305d95e4f766ec
0xtheMachine <aaronvsummers@gmail.com>
2020-08-30 22:12:26 +0100

    server setup

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3500;
+
+
+//-----------middleware-----------//
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        credentials: true,
+        origin: ['*'],
+        exposedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+    })
+);
+
+//-----------DATABASE-----------//
+mongoose.connect(`${process.env.DATABASE}`, {useNewUrlParser:true, useUnifiedTopology: true}, () => {
+    console.log('Connected to Database.')
+})
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
